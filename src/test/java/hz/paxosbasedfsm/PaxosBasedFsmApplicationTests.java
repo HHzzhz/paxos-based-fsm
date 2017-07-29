655791499004418b76a89a35d02bc5fdac8dafe9
huazheng <huazheng@lattebank.com>
2017-07-29 22:35:47 +0800

    init

---
+++ src/test/java/hz/paxosbasedfsm/PaxosBasedFsmApplicationTests.java
@@ -0,0 +1,16 @@
+package hz.paxosbasedfsm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaxosBasedFsmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
